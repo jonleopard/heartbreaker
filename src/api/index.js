250815b49b6cb9956f136ed6a7d711cdd264bcbb
Jon Leopard <jontron2000@gmail.com>
2018-07-30 18:20:50 +0200

    Committing progress

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,13 @@ import dotenv from 'dotenv';
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import passport from 'passport';
+import TwitterTokenStrategy from 'passport-twitter-token';
+import jwt from 'jsonwebtoken';
+import expressJwt from 'express-jwt';
 import request from 'request';
+import mongoose from 'mongoose';
+const User = mongoose.model('User');
 
 dotenv.config();
 
@@ -27,6 +33,46 @@ export default ({ config, db }) => {
     res.send('Hello World');
   });
 
+  passport.use(
+    new TwitterTokenStrategy(
+      {
+        consumerKey: process.env.CONSUMER_KEY,
+        consumerSecret: process.env.CONSUMER_SECRET,
+        includeEmail: true,
+      },
+      function(token, tokenSecret, profile, done) {
+        User.upsertTwitterUser(token, tokenSecret, profile, function(
+          err,
+          user
+        ) {
+          return done(err, user);
+        });
+      }
+    )
+  );
+
+  const createToken = function(auth) {
+    return jwt.sign(
+      {
+        id: auth.id,
+      },
+      'my-secret',
+      {
+        expiresIn: 60 * 120,
+      }
+    );
+  };
+
+  const generateToken = function(req, res, next) {
+    req.token = createToken(req.auth);
+    return next();
+  };
+
+  const sendToken = function(req, res) {
+    res.setHeader('x-auth-token', req.token);
+    return res.status(200).send(JSON.stringify(req.user));
+  };
+
   api.route('/auth/twitter/reverse').post(function(req, res) {
     request.post({
       url: 'https://api.twitter.com/oauth/request_token',
@@ -40,6 +86,86 @@ export default ({ config, db }) => {
       '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
     res.send(JSON.parse(jsonStr));
   });
+
+  api.route('/auth/twitter').post(
+    (req, res, next) => {
+      request.post(
+        {
+          url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
+          oauth: {
+            consumer_key: process.env.CONSUMER_KEY,
+            consumer_secret: process.env.CONSUMER_SECRET,
+            token: req.query.oauth_token,
+          },
+          form: { oauth_verifier: req.query.oauth_verifier },
+        },
+        function(err, r, body) {
+          if (err) {
+            return res.send(500, { message: err.message });
+          }
+
+          const bodyString =
+            '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+          const parsedBody = JSON.parse(bodyString);
+
+          req.body['oauth_token'] = parsedBody.oauth_token;
+          req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
+          req.body['user_id'] = parsedBody.user_id;
+
+          next();
+        }
+      );
+    },
+    passport.authenticate('twitter-token', { session: false }),
+    function(req, res, next) {
+      if (!req.user) {
+        return res.send(401, 'User Not Authenticated');
+      }
+
+      // prepare token for API
+      req.auth = {
+        id: req.user.id,
+      };
+
+      return next();
+    },
+    generateToken,
+    sendToken
+  );
+
+  //token handling middleware
+  const authenticate = expressJwt({
+    secret: 'my-secret',
+    requestProperty: 'auth',
+    getToken: function(req) {
+      if (req.headers['x-auth-token']) {
+        return req.headers['x-auth-token'];
+      }
+      return null;
+    },
+  });
+
+  const getCurrentUser = function(req, res, next) {
+    User.findById(req.auth.id, function(err, user) {
+      if (err) {
+        next(err);
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  };
+
+  const getOne = function(req, res) {
+    var user = req.user.toObject();
+
+    delete user['twitterProvider'];
+    delete user['__v'];
+
+    res.json(user);
+  };
+
+  api.route('/auth/me').get(authenticate, getCurrentUser, getOne);
 
   return api;
 };
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,71 +1,9 @@
-import dotenv from 'dotenv';
 import { Router } from 'express';
-import passport from 'passport';
-import jwt from 'jsonwebtoken';
-import expressJwt from 'express-jwt';
-import TwitterTokenStrategy from 'passport-twitter-token';
-import User from '../models/User';
-
-dotenv.config();
 
 export default ({ config, db }) => {
   let routes = Router();
 
   // add middleware here
-
-  // Passport
-  passport.use(
-    new TwitterTokenStrategy(
-      {
-        consumerKey: process.env.CONSUMER_KEY,
-        consumerSecret: process.env.CONSUMER_SECRET,
-        includeEmail: true,
-      },
-      function(token, tokenSecret, profile, done) {
-        User.upsertTwitterUser(token, tokenSecret, profile, function(
-          err,
-          user
-        ) {
-          return done(err, user);
-        });
-      }
-    )
-  );
-
-  // JWT
-
-  const createToken = function(auth) {
-    return jwt.sign(
-      {
-        id: auth.id,
-      },
-      'my-secret',
-      {
-        expiresIn: 60 * 120,
-      }
-    );
-  };
-
-  const generateToken = function(req, res, next) {
-    req.token = createToken(req.auth);
-    return next();
-  };
-
-  const sendToken = function(req, res) {
-    res.setHeader('x-auth-token', req.token);
-    return res.status(200).send(JSON.stringify(req.user));
-  };
-  //token handling middleware
-  const authenticate = expressJwt({
-    secret: 'my-secret',
-    requestProperty: 'auth',
-    getToken: function(req) {
-      if (req.headers['x-auth-token']) {
-        return req.headers['x-auth-token'];
-      }
-      return null;
-    },
-  });
 
   return routes;
 };
