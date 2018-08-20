f54eb88278ab859e4a95ee11d9dcd9b4b38dc7fb
Jon Leopard <jontron2000@gmail.com>
2018-08-20 12:41:25 +0200

    Package updates and formatting

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'rebass';
-import { injectGlobal } from 'styled-components'
+import { injectGlobal } from 'styled-components';
 
 injectGlobal`
   * { box-sizing: border-box; }
   body { margin: 0; background:#8d46e8 }
   min-height: 100vh
-`
-
+`;
 
 ReactDOM.render(
-   <Provider>
-       <App />
-   </Provider>, document.getElementById('root')
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
-
